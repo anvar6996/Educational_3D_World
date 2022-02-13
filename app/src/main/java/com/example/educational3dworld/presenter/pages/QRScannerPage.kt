caa1711d3ac8bdac0e52107378f2bdc7b81ef069
Anvar <juraquovanvar@mail.ru>
2022-02-13 18:18:25 +0500

    adapter object date add

--- app/src/main/java/com/example/educational3dworld/presenter/pages/QRScannerPage.kt
+++ app/src/main/java/com/example/educational3dworld/presenter/pages/QRScannerPage.kt
@@ -1,9 +1,7 @@
 package com.example.educational3dworld.presenter.pages
 
-import android.Manifest
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.example.educational3dworld.R
